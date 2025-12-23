ddfc12f06b2f83dc69636b8ae05ae2c818fe746f
Harald Schmitz-Becker <harald.schmitz-becker@dedalus.eu>
2025-12-23 10:41:07 +0100

    Import ok, noch ohne Parameter.

--- Logfile2Db.py
+++ Logfile2Db.py
@@ -8,34 +8,72 @@ conn = oracledb.connect(
     dsn="localhost/FREEPDB1"
 )
 
+# File
+
 file_path = Path(r"C:\Tickets\CUST1414766 verlangsamtes System\CUST1412353 Aufruf des DRG WP extrem langsam\DRGWP Aufruf KHVM langsam.log")
 customer_ticket_id = "CUST1412353"
 
-sql = """
-INSERT INTO perf_log (file_row, row_number, file_path, customer_ticket_id)
-VALUES (:file_row, :row_number, :file_path, :customer_ticket_id)
+sql_file = """
+INSERT INTO perf_file (
+        customer_ticket_id
+    ,   file_path
+    )
+    VALUES (
+        :customer_ticket_id
+    ,   :file_path
+    )
+    RETURNING id INTO :new_id
+"""
+
+new_id = conn.cursor().var(oracledb.NUMBER)
+
+with conn.cursor() as cur:
+    cur.execute(
+        sql_file,
+        customer_ticket_id = customer_ticket_id,
+        file_path = str(file_path),
+        new_id = new_id
+    )
+
+perf_file_id = int(new_id.getvalue()[0])
+
+# Rows
+
+sql_row = """
+INSERT INTO perf_row (
+        perf_file_id
+    ,   row_number
+    ,   file_row
+    )
+    VALUES (
+        :perf_file_id
+    ,   :row_number
+    ,   :file_row
+    )
 """
 
 rows = []
 row_number = 1
+
 with file_path.open(encoding="utf-8", errors="replace") as f:
     for line in f:
-        text = line.rstrip("\n")
-        # if not text:
-        #    continue   # leere Zeilen ignorieren
-
+        file_row = line.rstrip("\n")
+        if not file_row:
+            file_row = " "
         rows.append({
-            "file_row": text,
+            "perf_file_id": perf_file_id,
             "row_number": row_number,
-            "file_path": str(file_path),
-            "customer_ticket_id": customer_ticket_id
+            "file_row": file_row
         })
         row_number += 1
 
 
 # Batch-Insert → schnell!
 with conn.cursor() as cur:
-    cur.executemany(sql, rows)
+    cur.executemany(sql_row, rows)
+
+
+# Commit
 
 conn.commit()
 conn.close()
